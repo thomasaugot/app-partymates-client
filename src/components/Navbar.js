41c62bc36db09e7d7bc9e66ab2144b52f9865778
Thomas Augot <thomas.augot@hotmail.fr>
2023-02-13 18:17:57 +0100

    making Navbar responsive using Bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,54 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./../context/auth.context";
-import "./Navbar.css"
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Navbar.css";
 
-function Navbar() {
-
+function MyNavbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav className="Navbar">
-      <Link to="/">
-        <button className="navbarButton">Home</button>
-      </Link>
+    <>
+      <Navbar bg="transparent" expand="lg" className="Navbar" color="white">
+        <Container className="divNavbar">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/">
+              <button className="navbarButton">Home</button>
+              </Link>
+              <Link to="/events">
+              <button className="navbarButton">Upcoming Events</button>
+              </Link>
+            {isLoggedIn && (
+              <>
+                <Link to={`/profile/${user._id}`}>
+                <button className="navbarButton">My Profile</button>
+                </Link>
+                <button className="navbarButton" onClick={logOutUser}>
+                  Logout
+                </button>
+              </>
+            )}
 
-      <Link to="/events">
-        <button className="navbarButton">Upcoming Events</button>
-      </Link>
-
-      {isLoggedIn && (
-        <>
-        <Link to={`/profile/${user._id}`}><button className="navbarButton">My Profile</button></Link>
-        
-          <button className="navbarButton" onClick={logOutUser}>Logout</button>
-        </>
-      )}
-
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup"> <button className="navbarButton">Sign Up</button> </Link>
-          <Link to="/login"> <button className="navbarButton">Login</button> </Link>
-        </>
-      )}      
-    </nav>
+            {!isLoggedIn && (
+              <>
+                <Link to="/signup">
+                  <button className="navbarButton">Sign Up</button>{" "}
+                </Link>
+                <Link to="/login">
+                  <button className="navbarButton">Login</button>{" "}
+                </Link>
+              </>
+            )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
-export default Navbar;
+export default MyNavbar;
