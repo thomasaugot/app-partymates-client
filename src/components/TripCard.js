67e5113fae4dd20e62e2b0354de767811905f688
Thomas Augot <thomas.augot@hotmail.fr>
2023-02-15 18:36:00 +0100

    responsiveness tablets done

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -6,28 +6,33 @@ import dayjs from "dayjs";
 import MessageForm from "../components/MessageForm";
 
 function TripCard(props) {
-
   const { tripId, eventId } = useParams();
   const { user } = useContext(AuthContext);
 
   const content = props.tripMessage;
 
   return (
-    <div className="tripCardEventDetails">
-      <h4 className="tripCardEventDetailsH4">By {props.creatorName}</h4>
-      <h4 className="tripCardEventDetailsH4">Message:</h4>
-      <p className="tripCardEventDetailsP">{props.tripMessage}</p>
-      <p>Posted on {dayjs(props.creationDate).format("MMM D, YYYY h:mm A")}</p>
-      {props.creatorId === user?._id && (
-        <Link to={ `/events/${eventId}/share-your-trip/${props.tripId}/edit`} state= {{content:content} }
-          className="editBtn"
-        >
-          Edit post
-        </Link>
-      )}
-      {props.creatorId !== user?._id && (
-        <MessageForm creator={user} recipient={props.creatorId} />
-      )}
+    <div className="tripcardPage">
+      <div className="tripCardEventDetails">
+        <h4 className="tripCardEventDetailsH4">By {props.creatorName}</h4>
+        <h4 className="tripCardEventDetailsH4">Message:</h4>
+        <p className="tripCardEventDetailsP">{props.tripMessage}</p>
+        <p>
+          Posted on {dayjs(props.creationDate).format("MMM D, YYYY h:mm A")}
+        </p>
+        {props.creatorId === user?._id && (
+          <Link
+            to={`/events/${eventId}/share-your-trip/${props.tripId}/edit`}
+            state={{ content: content }}
+            className="editBtn"
+          >
+            Edit post
+          </Link>
+        )}
+        {props.creatorId !== user?._id && (
+          <MessageForm creator={user} recipient={props.creatorId} />
+        )}
+      </div>
     </div>
   );
 }
